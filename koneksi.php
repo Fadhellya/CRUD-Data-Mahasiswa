e21295f8383cd74d559a13a0ea907a4e806fd5c3
Fadhellya <113502315+Fadhellya@users.noreply.github.com>
2024-10-08 05:38:43 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,14 @@
+<?php
+    $hostname = getenv('DB_HOST');
+    $username = getenv('DB_USER');
+    $password = getenv('DB_PASS');
+    $database = getenv('DB_NAME');
+
+    $connect = mysqli_connect($hostname, $username, $password, $database);
+
+    // Cek koneksi
+    if (!$connect) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+?>
 
