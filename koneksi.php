1c4c7ddd8f6eccceb7daec43930da57f6209a36e
Fadhellya <113502315+Fadhellya@users.noreply.github.com>
2025-03-25 13:23:28 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,9 @@
     $username = getenv('DB_USER') ? getenv('DB_USER') : 'root';
     $password = getenv('DB_PASS') ? getenv('DB_PASS') : '';
     $database = getenv('DB_NAME') ? getenv('DB_NAME') : 'akademik';
-    $connect = mysqli_connect($hostname, $username, $password, $database);
+    $port     = getenv('DB_PORT') ? getenv('DB_PORT') : '3306'; // Default ke 3306 jika tidak ada env
+    $connect = mysqli_connect($hostname, $username, $password, $database, $port);
+
     // Cek koneksi
     if (!$connect) {
         die("Connection failed: " . mysqli_connect_error());
